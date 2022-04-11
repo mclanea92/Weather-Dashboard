d269ca58709552db82e3d30024d3bc1a85a3b324
Adam McLane <mclanea92@gmail.com>
2022-04-11 18:33:17 -0400

    updated javascript

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,31 @@
-var APIkey = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=b593b8f7d2b3fb75befb732897df7d93"
-var cityName = ('.citynameinput')
+var cityName = document.querySelector('.citynameinput')
+var APIkey = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=b593b8f7d2b3fb75befb732897df7d93"
+var todayWeather = document.querySelector('.todayWeather')
+var citySearch = document.querySelector('.citySearch')
+
+// var test = "https://api.openweathermap.org/data/2.5/weather?q=philadelphia&units=imperial&appid=b593b8f7d2b3fb75befb732897df7d93"
+
+// fetch(test).then(function(response){
+//     return response.json()
+// }).then(function(data){
+//     console.log(data.main.temp)
+//     var pTag = document.createElement('p')
+//     pTag.innerHTML = data.main.temp
+//     todayWeather.append(pTag)
+// })
+
+function CitySeachFunction(){
+    var cityName = document.querySelector('.citynameinput').value
+var APIkey = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=b593b8f7d2b3fb75befb732897df7d93"
+fetch(APIkey).then(function(response){
+    return response.json()
+}).then(function(data){
+    console.log(data.main.temp)
+    var pTag = document.createElement('p')
+    pTag.innerHTML = data.main.temp
+    todayWeather.append(pTag)
+})
+}
+
+
+citySearch.addEventListener('click', CitySeachFunction)
