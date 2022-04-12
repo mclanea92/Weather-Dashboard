b31a2f39b4f8f6006098ed436e4f9f414b9169b2
Adam McLane <mclanea92@gmail.com>
2022-04-12 14:29:04 -0400

    updated javascript and html

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,12 @@ var APIkey = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "
 var todayWeather = document.querySelector('.todayWeather')
 var citySearch = document.querySelector('.citySearch')
 
+var test = "https://api.openweathermap.org/data/2.5/weather?q=philadelphia&units=imperial&appid=b593b8f7d2b3fb75befb732897df7d93"
+// used to find what data i need.
+fetch(test)
+.then(res => res.json())
+    .then(data => console.log(data))
+
 
 function CitySeachFunction(){
     var cityName = document.querySelector('.citynameinput').value
@@ -11,11 +17,26 @@ fetch(APIkey).then(function(response){
     return response.json()
 }).then(function(data){
     console.log(data.main.temp)
-    var pTag = document.createElement('p')
-    pTag.innerHTML = data.main.temp
-    todayWeather.append(pTag)
+    var currentTemp = document.getElementById('#currentTemp')
+    currentTemp = data.main.temp
+    todayWeather.append(Math.floor(currentWeather))
 })
 }
 
+// function CitySeachFunction(){
+//     var cityName = document.querySelector('.citynameinput').value
+// var APIkey = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=b593b8f7d2b3fb75befb732897df7d93"
+// fetch(APIkey).then(function(response){
+//     return response.json()
+// }).then(function(data){
+//     console.log(data.main.humidity)
+//     var currentHumid = document.getElementById('#currentHumid')
+//     currentHumid = data.main.humidity
+//     todayWeather.append(currentHumid)
+// })
+// }
 
-citySearch.addEventListener('click', CitySeachFunction)
+
+citySearch.addEventListener('click', CitySeachFunction) 
+   
+
