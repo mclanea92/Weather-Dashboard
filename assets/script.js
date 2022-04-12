e2060dc4b56b18658591d38e74e83e8a5d4d5c68
Adam McLane <mclanea92@gmail.com>
2022-04-12 13:11:14 -0400

    updated javascript

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,6 @@ var APIkey = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "
 var todayWeather = document.querySelector('.todayWeather')
 var citySearch = document.querySelector('.citySearch')
 
-// var test = "https://api.openweathermap.org/data/2.5/weather?q=philadelphia&units=imperial&appid=b593b8f7d2b3fb75befb732897df7d93"
-
-// fetch(test).then(function(response){
-//     return response.json()
-// }).then(function(data){
-//     console.log(data.main.temp)
-//     var pTag = document.createElement('p')
-//     pTag.innerHTML = data.main.temp
-//     todayWeather.append(pTag)
-// })
 
 function CitySeachFunction(){
     var cityName = document.querySelector('.citynameinput').value
